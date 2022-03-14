a69db50a655f2724ac139c6fed50e34c310380e9
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2022-03-14 08:35:37 +0000

    add コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  # resourcesメソッドは、ルーティングを一括して自動生成
+  # onlyは、生成するルーティングを限定するオプション
+  
+  resources :users, only: [:show]
+  
 end
